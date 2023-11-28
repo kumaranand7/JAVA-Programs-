ba00ba4ffa9a5bb1b7015836997235dfca550176
kumaranand7 <kumarguptaanand1@gmail.com>
2023-11-28 21:37:13 +0530

    Coommit

--- ExceptionFileNotFoundEg.java
+++ ExceptionFileNotFoundEg.java
@@ -7,15 +7,19 @@ public class ExceptionFileNotFoundEg {
     public static void main(String[] args) {
         try {
             // Specify the file path
-            String filePath = "C:\\Users\\Anand\\OneDrive\\Desktop\\abc.txst";
+            String filePath = "C:\\Users\\Anand\\OneDrive\\Desktop\\new_file.txt";  //if file does not exist a new file will be created (refresh)
 
             // Create a PrintWriter object with the specified file path
             PrintWriter pw = new PrintWriter(filePath);
 
             // Write some text to the file
-            pw.println("Hello My name is Anand: ");
+            for(int i=1; i<=10; i++){
 
-            // Close the PrintWriter to ensure that the data is flushed and the file is properly closed
+                pw.println(i+ " Hello My name is Anand:");
+                pw.println(i+" My name is Harry: ");
+                pw.println();
+            }
+            // Close the PrintWriter to ensure that the data is flushed(data goes to buffer(temp storage) then permanent storage) and the file is properly closed
             pw.close();
 
             System.out.println("Data written to the file successfully.");
