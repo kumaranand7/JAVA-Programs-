412b3ef9d9050e6af523ece25750c25989f6b1fb
kumaranand7 <kumarguptaanand1@gmail.com>
2024-05-25 12:30:49 +0530

    Anagram program

---
+++ AnagramStirng.java
@@ -0,0 +1,36 @@
+
+// find out if the given two strings are anagrams or not?
+
+import java.util.Arrays;
+
+public class AnagramStirng {
+
+
+    public static boolean checkAnagram(String str1,String str2){
+
+
+        str1=str1.toLowerCase();
+        str2=str2.toLowerCase();
+  
+        char ch1 []=str1.toCharArray();
+        char ch2 []=str2.toCharArray();
+  
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+  
+       return Arrays.equals(ch1, ch2);
+
+
+    }
+
+    public static void main(String[] args) {
+        
+    String str1="HEART";
+    String str2="EARTH";
+
+      boolean result=checkAnagram(str1,str2);
+      System.out.println(result);
+
+    }
+    
+}
--- PalindromCheck.java
+++ PalindromCheck.java
@@ -1,42 +1,34 @@
 // How do you determine if a string is a palindrome?
 
-
 import java.util.*;
+
 public class PalindromCheck {
 
+    public static String reverseString(String str) {
 
-    public static String reverseString(String str){
-
-     
-        String nstr="";
-        for(int i=str.length()-1; i>=0; i--){
-            nstr= nstr+str.charAt(i);
-                }
-                return nstr;
+        String nstr = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            nstr = nstr + str.charAt(i);
+        }
+        return nstr;
     }
 
-    public static boolean checkPlaindrom(String reverse,String str  ){
-
+    public static boolean checkPlaindrom(String reverse, String str) {
 
         return str.equals(reverse);
     }
 
     public static void main(String[] args) {
-        
-        Scanner sc= new Scanner(System.in);
+
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the any string:");
-        String str=sc.nextLine();
+        String str = sc.nextLine();
 
-        
+        String reverse = reverseString(str);
 
-          String reverse= reverseString(str);
-          
-          Boolean result= checkPlaindrom(reverse,str);
-          System.out.println(result);
+        Boolean result = checkPlaindrom(reverse, str);
+        System.out.println(result);
 
-     
-
-        
     }
 
 }
