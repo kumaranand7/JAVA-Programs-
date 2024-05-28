89f9d9493551c6e366b54d56f1607c503b04d3d8
kumaranand7 <kumarguptaanand1@gmail.com>
2024-05-28 22:45:29 +0530

    reverse array

---
+++ ArrayReverse.java
@@ -0,0 +1,29 @@
+//reverse array using two pointer approach
+
+public class ArrayReverse {
+
+    public static void main(String[] args) {
+        
+        int arr[]={7,4,5,8,99,3,44,1};
+
+        int start=0;
+        int end=arr.length-1;
+
+        while(start<end){
+
+            int tmp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=tmp;
+
+            start++;
+            end--;
+        }
+
+        System.out.println("Reverse array is: ");
+        for (int i : arr) {
+            System.out.print(i+ " ");
+            
+        }
+    }
+    
+}
