a04c71a983da4f790360efb7afd1192fc615e85e
kumaranand7 <kumarguptaanand1@gmail.com>
2024-05-30 19:00:19 +0530

    second-largest

---
+++ SecondLargest.java
@@ -0,0 +1,31 @@
+public class SecondLargest {
+
+    // Method to find the largest element in the array
+    public static int largest(int[] arr) {
+        int MAX_ELEMENT = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) { 
+            if (arr[i] > MAX_ELEMENT) {
+                MAX_ELEMENT = arr[i];
+            }
+        }
+        return MAX_ELEMENT;
+    }
+
+    // Method to find the second largest element in the array
+    public static int secondLargest(int[] arr, int max_element) {
+        int s_MAX_ELEMENT = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {  
+            if (arr[i] > s_MAX_ELEMENT && arr[i] < max_element) { 
+                s_MAX_ELEMENT = arr[i];
+            }
+        }
+        return s_MAX_ELEMENT;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {7, 4, 555, 8, 99, 2};
+        int max_element = largest(arr);
+        System.out.println("Largest element is " + max_element);
+        System.out.println("Second Largest element is " + secondLargest(arr, max_element));
+    }
+}
