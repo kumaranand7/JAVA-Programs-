0a6d3815afefa1e715be97c4abe5d9abbd96bae0
kumaranand7 <kumarguptaanand1@gmail.com>
2024-06-09 12:43:33 +0530

    Prime

---
+++ CheckPrime.java
@@ -0,0 +1,23 @@
+public class CheckPrime {
+    public static void main(String[] args) {
+        int n = 3;
+        boolean isPrime = true;
+
+        if (n <= 1) {
+            isPrime = false; 
+        } else {
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    isPrime = false;
+                    break; 
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println("Prime");
+        } else {
+            System.out.println("Not prime");
+        }
+    }
+}
