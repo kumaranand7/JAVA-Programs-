dacc7aa6f93727aa9e3f653feebc25ea9e76f321
Anand Kumar Gupta <96008269+kumaranand7@users.noreply.github.com>
2024-08-11 19:23:24 +0530

    Update CheckPrime.java

--- CheckPrime.java
+++ CheckPrime.java
@@ -7,7 +7,7 @@ public class CheckPrime {
             isPrime = false; 
         } else {
             for (int i = 2; i <= Math.sqrt(n); i++) {
-                if (n % i == 0) { 
+                if (n % i == 0) {  
                     isPrime = false;
                     break; 
                 }
