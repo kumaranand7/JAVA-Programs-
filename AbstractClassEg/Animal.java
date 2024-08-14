9664927301d7b40ef03f1aa7ec7c24f3db902057
Anand Kumar Gupta <96008269+kumaranand7@users.noreply.github.com>
2024-08-14 19:50:55 +0530

    Update Animal.java

--- AbstractClassEg/Animal.java
+++ AbstractClassEg/Animal.java
@@ -3,7 +3,7 @@ package AbstractClassEg;
 abstract class Animal {
 
     public abstract void sound(); // abstract class
-
+ 
     public void sleep() {
         System.out.println("The animal is sleeping"); // non abstract class
 
